38185cea6a79faed09226fca6bc98829ae6f0c99
darkrilin <darkrilin@users.noreply.github.com>
2022-03-21 19:04:10 +1100

    Improved dead grass coloring

--- src/main/java/net/sewerbunny/glitchygrowth/GlitchyGrowthClient.java
+++ src/main/java/net/sewerbunny/glitchygrowth/GlitchyGrowthClient.java
@@ -17,11 +17,28 @@ public class GlitchyGrowthClient implements ClientModInitializer {
                 ModBlocks.DEAD_GRASS, ModBlocks.DEAD_GRASS_BLOCK);
 
         // Tint blocks
-        ColorProviderRegistry.BLOCK.register((state, view, pos, tintIndex) -> 0xbba577, ModBlocks.DEAD_GRASS);
-        ColorProviderRegistry.BLOCK.register((state, view, pos, tintIndex) -> 0xbba577, ModBlocks.DEAD_GRASS_BLOCK);
+        ColorProviderRegistry.BLOCK.register((state, view, pos, tintIndex) ->
+                view != null && pos != null ? getDeadGrassColor(view, pos) : 0xbba577, ModBlocks.DEAD_GRASS);
+        ColorProviderRegistry.BLOCK.register((state, view, pos, tintIndex) ->
+                view != null && pos != null ? getDeadGrassColor(view, pos) : 0xbba577, ModBlocks.DEAD_GRASS_BLOCK);
 
         // Tint items
         ColorProviderRegistry.ITEM.register((stack, tintIndex) -> 0xbba577, ModBlocks.DEAD_GRASS);
         ColorProviderRegistry.ITEM.register((stack, tintIndex) -> 0xbba577, ModBlocks.DEAD_GRASS_BLOCK);
+    }
+
+    public int getDeadGrassColor(BlockRenderView view, BlockPos pos) {
+        int grassColor = BiomeColors.getGrassColor(view, pos);
+        int deadColor = 0xbba577;
+
+        int r = avgColorChannel((grassColor >> 16) & 0xff, (deadColor >> 16) & 0xff);
+        int g = avgColorChannel((grassColor >> 8) & 0xff, (deadColor >> 8) & 0xff);
+        int b = avgColorChannel(grassColor & 0xff, deadColor & 0xff);
+
+        return (0xff << 24) | ((r & 0xff) << 16) | ((g & 0xff) << 8) | (b & 0xff);
+    }
+
+    public int avgColorChannel(int channel1, int channel2) {
+        return (int) Math.pow((Math.pow(channel1, 2.2) + Math.pow(channel2, 2.2) * 3) / 4, 1 / 2.2);
     }
 }
